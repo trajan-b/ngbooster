4a1fc7db0f1756dd5c40418455af0358cb00e26c
Trajan Bronner <trajan.bronner@gmail.com>
2017-05-03 20:25:26 +0200

    [Templates] Updating spec templates

--- templates/spec-store-template.spec.ts
+++ templates/spec-store-template.spec.ts
@@ -2,19 +2,31 @@ import * as angular from 'angular';
 
 describe('<%=camelNameFirstUpper%>Store', () => {
 
-    let _httpBackend: angular.IHttpBackendService;
+    let _$httpBackend: angular.IHttpBackendService;
     let _<%=camelName%>Store: <%=camelNameFirstUpper%>Store;
 
     beforeEach(inject((
         $httpBackend: angular.IHttpBackendService,
         <%=camelName%>Store: <%=camelNameFirstUpper%>Store
     ) => {
-        _httpBackend = $httpBackend;
+        _$httpBackend = $httpBackend;
         _<%=camelName%>Store = <%=camelName%>Store;
     }));
 
-    xit('should ...', () => {
+    afterEach(() => {
+        _$httpBackend.verifyNoOutstandingExpectation();
+        _$httpBackend.verifyNoOutstandingRequest();
+    });
+    <!--begin-GET
+    it('should test HTTP GET call on /<%=camelName%> route', () => {
+
+        _$httpBackend.expectGET('/<%=camelName%>').and.respond({});
+        let result =_<%=camelName%>Store.get<%=camelNameFirstUpper%>();
+        _$httpBackend.flush();
+
+        expect(result).toEqual();
 
     });
+    end-GET-->
 
 });
--- templates/spec-template.spec.ts
+++ templates/spec-template.spec.ts
@@ -1,3 +1,27 @@
-<%=dashName%>
-<%=camelName%>
-<%=camelNameFirstUpper%>
+import * as angular from 'angular';
+import 'angular-mock';
+
+describe('<%=camelNameFirstUpper%>Component', () => {
+
+    let _$compile: angular.ICompileService;
+    let _$httpBackend: angular.IHttpBackendService;
+    let _scope: angular.IRootScopeService;
+
+    interface I<%=camelNameFirstUpper%>Component extends angular.IController, <%=camelNameFirstUpper%>Component {
+
+    }
+
+    beforeEach(angular.mock.module(<%=camelNameFirstUpper%>.name));
+
+    beforeEach(inject((
+        $compile: angular.ICompileService,
+        $httpBackend: angular.IHttpBackendService,
+        $rootScope: angular.IRootScopeService,
+        <%=camelName%>Store: <%=camelNameFirstUpper%>Store
+    ) => {
+        _$compile = $compile;
+        _$httpBackend = $httpBackend;
+        _scope = $rootScope.new() as I<%=camelNameFirstUpper%>Component;
+    }));
+
+});
